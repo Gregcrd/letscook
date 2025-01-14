8d37dfeb517387016ebae4daca57ced71261d127
Gregcrd <schiltzgregoire@gmail.com>
2025-01-14 14:10:37 +0100

    ok

---
+++ README.md
@@ -0,0 +1,42 @@
+# Let's Cook 
+C'est un site/application web simple permettant de faire des recherches parmi une lise de 30 recettes.
+
+## Fonctionnalités
+-Recherche par mot-clé d'une recette et par le biais d'un formulaire 
+-Structure d'une carte de restaurant virtuelle fonctionnelle 
+-possibilité de choisir le nombre de recette à afficher 
+-permet de retrouver l'historique des recherches effectuées
+
+
+
+### Objectifs pédagogiques
+
+Utilisation de fonctions dites asynchrones pour interagir avec une API.
+Mettre en pratique la programmation orientée objet.
+utilisation d'un css globale et utilitaire 
+
+### Ressources utilisées
+
+- **API :** [DummyJSON](https://dummyjson.com/) pour les données des recettes.
+- **Vanilla JavaScript :** .
+- **CSS :** personnalisé
+
+### Explications techniques 
+
+Le chargement des données se fait depuis l'Api en utilisant une fonction ASYNC FETCH
+une fois recuperé , les données sont affichées sous forme de carte .
+un message d'erreur permet d'indiquer à l'utilisateur si sa requete echoue tout en respectant les codes user friendly.
+
+### Deploiement 
+Néant à ce jour 
+
+### Auteur
+Nom : Schiltz Grégoire
+Formation : DWWWM
+Onjectif : Validation des compétences en création d'applications web
+
+### Améliorations possibles 
+
+1.Ajout des recettes par l'utilisateur
+2.Trie des recettes par origine du plat .
+
--- assets/js/correctionDeMonIndex.js
+++ assets/js/correctionDeMonIndex.js
@@ -1,94 +1,114 @@
 console.log("script loaded !");
-
+ 
 const container = document.querySelector(".recipes");
 // selectionner les element de filtres : input, select
-
+ 
 console.log("container:", container);
-
+ 
 // fetch('https://dummyjson.com/recipes')
 // .then(res => res.json())
 // .then(console.log);
-
+ 
 // fonction fetch c'est une fonction async ici c est celle pour recuper dans api données json
-const input = document.querySelector("#searchInput");
-const limitSelect = document.querySelector("#limitSelect");
-let userKeyword = "";
-let recipeLimit = 10; // Valeur 10 recettes
+const searchBarElement = document.querySelector("#searchInput");
+const selectElement = document.querySelector("#limitSelect");
 
-async function fetchRecettes(kw ='', limit = 10) {
+
+let currentKeyWord = "";
+let currentFilter = "" ;
+let currentLimit = "" ;
+ 
+ 
+ 
+async function fetchRecettes(keyWord ='',  filterSortBy = 'sortBy=id&order=desc' ,limit = '10') {
   try {
-    let userQuery = `/search?q=${kw}`;
-    const responseServeur = await fetch(
-      `https://dummyjson.com/recipes${userQuery}`
-    );
+    const queryKeyWord = keyWord ? `/search?q=${keyWord}` : "";
+    const queryFilter = filterSortBy
+      ? `${queryKeyWord ? "&" : "?"}${filterSortBy}`
+      : "";
+    const queryLimit = limit
+      ? `${queryKeyWord || queryFilter ? "&" : "?"}limit=${limit}`
+      : "";
 
+      
+      const url = `https://dummyjson.com/recipes${queryKeyWord}${queryFilter}${queryLimit}`;
+
+      console.log('url:', url)
+    const responseServeur = await fetch(url);
+   
+   
+ 
     if (!responseServeur.ok) {
       throw new Error("erreur lors de la recuperation json");
     }
-
+ 
     const response = await responseServeur.json();
-
-    return response.recipes.slice(0,limit);
+   
+   return response
   } catch (error) {
     console.error("Une erreur s'est produite : ", error);
   }
 }
 // ici fonction pour trier les recettes par ordre d'id
-
+ 
 // async function fetchRecettesTrie() {
 //   const recettes = await fetchRecettes();
 //   if (!recettes) {
 //     throw new Error("pas de données disponibles pour le tri");
 //   }
-
+ 
 //   return recettesTriees;
 // }
-
+ 
 // ici pour afficher les elements
+ 
+async function affichageRecette(keyWord , filterSortBy , limit) {
+  container.innerHTML="chargement en cours...";
 
-async function affichageRecette(kw) {
-  container.innerHTML="chargement en cours..."
-  const data = await fetchRecettes(kw , recipeLimit) ;
+  const data = await fetchRecettes(keyWord , filterSortBy , limit) ;
   console.log(data);
+  
   if (!data || data.length === 0) {
     container.innerHTML = "<p>Aucune recette trouvée.</p>";
     return;
 }
   container.innerHTML="";
-  data.forEach(function (element) {
+  data.recipes.forEach(function (element) {
     const article = document.createElement("article");
-
+ 
     // console.log(element);
     article.innerHTML += `
+    <img src="${element.image}"  alt="${element.name}">
             <h3>${element.name}</h3>
             <p>${element.ingredients}</p>
-            
-        `
+           
+        `;
     container.appendChild(article);
   });
-  
+ 
 }
-
+ 
 affichageRecette();
 // test recherche repas
-
-
-
-input.addEventListener("input", function (event) {
- userKeyword = event.target.value;
-
+ 
+ 
+ 
+searchBarElement.addEventListener("input", function (event) {
+ currentKeyWord = event.target.value;
+ 
   // if (!userKeyword) {
   //   alert("Veuillez saisir un mot dans la liste pour rechercher une recette.");
   //   return;
   // }
   // Construire l'URL pour l'API
-  var userQuery = "https://dummyjson.com/recipes/search?q=" + userKeyword;
+  var userQuery = "https://dummyjson.com/recipes/search?q=" + currentKeyWord;
   console.log("URL générée :", userQuery);
-
+ 
   // Appeler l'API avec l'URL
-  affichageRecette(userKeyword);
+  affichageRecette(currentKeyWord);
 });
 limitSelect.addEventListener("change", function (event) {
-  recipeLimit = parseInt(event.target.value, 10); // Mettre à jour la limite
-  affichageRecette(userKeyword); // Recharger les recettes avec la nouvelle limite
+ 
+  affichageRecette(currentKeyWord); // Recharger les recettes avec la nouvelle limite
 });
+ 
